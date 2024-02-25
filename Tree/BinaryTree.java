aa212eb5a273cc74e93590342eb19b2d779f99ef
Betisor Sorin <s.betisor@student.maastrichtuniversity.nl>
2024-02-25 22:57:46 +0100

    traversals BS pre/post

--- Tree/BinaryTree.java
+++ Tree/BinaryTree.java
@@ -9,30 +9,6 @@ public class BinaryTree<T> implements BinaryTreeADT<T> {
     }
 
     @Override
-    public boolean checkBST(Node<T> root, int min, int max) {
-        try {
-            if (root == null){
-                return true;}
-
-            if ((Integer) root.data < min || (Integer) root.data > max){
-                return false;}
-
-            return (checkBST(root.left, min, (Integer) root.data - 1) && checkBST(root.right, (Integer) root.data + 1, max)); //no equal values allowed
-
-        } 
-        catch (Exception e) // In case of root.data not being an integer
-        {
-            System.out.println("Data type not supported");
-            return false;
-        }
-    }
-
-    @Override
-    public boolean checkBSTWrapper(Node<T> root) {
-        return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE); // Assuming Integers
-    }
-
-    @Override
     /*
      * In order traversal:
      * The left subtree is traversed first
@@ -48,6 +24,28 @@ public class BinaryTree<T> implements BinaryTreeADT<T> {
         System.out.println(root.data);
 
         traverseInOrder(root.right);
+    }
+
+    public void traversePreOrder(Node<T> root) {
+        if (root == null)
+            return;
+
+        System.out.println(root.data);
+
+        traversePreOrder(root.left);
+
+        traversePreOrder(root.right);
+    }
+
+    public void traversePostOrder(Node<T> root) {
+        if (root == null)
+            return;
+
+        traversePostOrder(root.left);
+
+        traversePostOrder(root.right);
+
+        System.out.println(root.data);
     }
 
     @Override
@@ -120,6 +118,32 @@ public class BinaryTree<T> implements BinaryTreeADT<T> {
                 subtree_delete(successor);
             }
         }
+    }
+
+    @Override
+    public boolean checkBST(Node<T> root, int min, int max) {
+        try {
+            if (root == null) {
+                return true;
+            }
+
+            if ((Integer) root.data < min || (Integer) root.data > max) {
+                return false;
+            }
+
+            return (checkBST(root.left, min, (Integer) root.data - 1)
+                    && checkBST(root.right, (Integer) root.data + 1, max)); // no equal values allowed
+
+        } catch (Exception e) // In case of root.data not being an integer
+        {
+            System.out.println("Data type not supported");
+            return false;
+        }
+    }
+
+    @Override
+    public boolean checkBSTWrapper(Node<T> root) {
+        return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE); // Assuming Integers
     }
 
 }
